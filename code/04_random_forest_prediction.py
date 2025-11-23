a3ecedda7c32300692874877445a72d4845c7232
saeek587 <saeesuryakant.kurhade@edhec.com>
2025-11-23 14:58:24 +0100

    Update 04_random_forest_prediction.py

--- code/04_random_forest_prediction.py
+++ code/04_random_forest_prediction.py
@@ -6,50 +6,58 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+
+
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset (update path accordingly)
-# dataset = pd.read_csv("factored_table.csv")
+dataset = pd.read_excel("C:\Users\SAEE KURHADE\Desktop\Stats Project\Markov_Random_Forest.xlsx")
 
 # Replace with your data loading code
 # Example:
-# ds2 = dataset.replace({
-#   "LR/lc": "LC",
-#   "LR/cd": "LC",
-#   "LR/nt": "NT",
-#   "CR(PE)": "CR",
-#   "CR(PEW)": "CR",
-#   "EW": "EX",
-#   "NR": "DD"
-# })
+ds2 = dataset.replace([ "LR/lc","LC","LR/cd","LR/nt","CR(PE)","CR(PEW)","EW","NR","DD"],['LC','LC','NT','CR','CR','EX','LC','LC'])
 
 # Drop columns not needed
-# ds2 = ds2.drop(columns=["Scientific name", "Common name"])
+ds2 = ds2.drop(columns=["Scientific name", "Common name"])
+#using ordinal encoder on response vector
+from sklearn.preprocessing import OrdinalEncoder
+enc = OrdinalEncoder(categories= [[ ’LC’ , ’NT’ , ’VU’ , ’EN’ , ’CR’ , ’EX’]])
+print(ds2 [ 'IUCN Red List (2021) ']. value counts ())
+print(ds2 [ 'IUCN Red List (2022) ']. value counts ())
+f1 = enc.fit_transform(ds2. iloc [: ,[1]])
+f1 = f1.reshape(2024,)
 
 # One-hot encode taxonomy and previous status
-# X = pd.concat([pd.get_dummies(ds2.iloc[:,0]), pd.get_dummies(ds2.iloc[:,1])], axis=1)
-# y = ds2["IUCN Red List 2022"]
+ds4 = pd.get_dummies(ds3 [0])
+ds5 = pd.get_dummies(ds3 [1])
+ds3 [ds4.columns]=ds4
+ds4 [ds5.columns]=ds5
+ds4 [res.columns] = res
+ds3.drop(0,axis=1,inplace=True)
+ 
+y = ds4['IUCN Red List (2022)']
+ds4.drop('IUCN Red List (2022)' , axis=1,inplace=True)
+X = ds4
 
 # Train/test split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
 # Model training
-# classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-# classifier.fit(X_train, y_train)
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, criterion= 'gini',random_state=0)
+classifier.fit(X_train, y_train)
 
 # Predictions
-# y_pred = classifier.predict(X_test)
+y_pred = classifier.predict(X_test)
+pred = classifier.predict(X_train)
+
 
 # Evaluation
+
+from sklearn.metrics import classification_report, confusion_matrix
 # print(confusion_matrix(y_test, y_pred))
 # print(classification_report(y_test, y_pred))
 
-# Feature importance
-# feature_imp = pd.Series(classifier.feature_importances_, index=X.columns).sort_values(ascending=False)
-# sns.barplot(x=feature_imp[:10], y=feature_imp.index[:10])
-# plt.title('Top 10 Feature Importances')
-# plt.show()
